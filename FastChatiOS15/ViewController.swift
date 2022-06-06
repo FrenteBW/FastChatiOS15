65216bd72a67f18354c60170f0667527e90ba60c
An Byungwook <abw2619@icloud.com>
2022-06-06 13:43:40 +0900

    Initial Commit

---
+++ FastChatiOS15/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FastChatiOS15
+//
+//  Created by 안병욱의 mac on 2022/06/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
